60f48c6e9e1de5a366af6b450215c4e914f7c7a9
seannathan95 <sean_nathan@brown.edu>
2020-05-11 01:13:42 -0400

    message

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,8 +1,3 @@
-from .loadDataSet import (load_gunpoint)
+from .loadDataSet import (load_gunpoint, _load_dataset, _load_ucr_dataset)
 
-from .ucr import ucr_dataset_info, ucr_dataset_list, fetch_ucr_dataset
-
-__all__ = ['load_basic_motions', 'load_coffee', 'load_gunpoint',
-           'load_pig_central_venous_pressure', 'make_cylinder_bell_funnel',
-           'ucr_dataset_info', 'ucr_dataset_list', 'fetch_ucr_dataset',
-           'uea_dataset_info', 'uea_dataset_list', 'fetch_uea_dataset']
+__all__ = ['load_gunpoint', "_load_dataset", "_load_ucr_dataset"]
